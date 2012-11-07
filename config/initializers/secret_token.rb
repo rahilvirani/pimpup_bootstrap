382ce4b694367134d8d420a23c6188bcb0a92d71
Rahil Virani <rahil@Rahils-MacBook-Air.local>
2012-11-08 00:59:16 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pimpup::Application.config.secret_token = '92c64a5c469a64c08f3242eb5f3c283ae18f30d51a7928b8963c1a80b4d080c3904fae8d80210897ad8e3a1016236f98ddaa28485e952caa9840012cef9c282d'
